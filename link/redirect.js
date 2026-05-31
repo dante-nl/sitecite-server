e807392d47e37aca9ebc075c8037ee09c8fa4f81
dante_nl <61943433+dante-nl@users.noreply.github.com>
2026-05-31 19:46:58 +0200

    Added URLhaus
    Every redirect is now tryed against the URLhaus database (if the config allows for it) and if a link might be harmful, the user will be displayed a warning

--- link/redirect.js
+++ link/redirect.js
@@ -42,29 +42,53 @@ async function redirectUser(req, res) {
     }
 
     const url = linkReq[0].link
-
-    // TODO: check if link is bad
-    // if link bad: 
-    // 1. remove auto redirect
-    // 2. replace default text
-    // 3. only allow when user clicks
-
+        
     const filePath = path.join(process.cwd(), '/link/link.html');
     let content = await fs.readFile(filePath, 'utf8');
-
+    
     const host = new URL(url).hostname
-
+    
     content = content.replace(/\{code\}/g, code);
     content = content.replace(/\{baseUrl\}/g, process.env.HOST);
     content = content.replace(/\{url\}/g, url);
     content = content.replace(/\{host\}/g, host);
     content = content.replace(/\{text\}/g, linkReq[0].text);
+    
+    if (process.env.ABUSE_CH_KEY.length > 5) {
+        // clearly there's a valid urlhaus key
+        try {
+            // try and see if we can screen it
+            const data = new URLSearchParams();
+            data.append('url', url);
 
+            const urlHausReq = await fetch("https://urlhaus-api.abuse.ch/v1/url/", {
+                method: "post",
+                headers: {
+                    "Auth-Key": process.env.ABUSE_CH_KEY,
+                },
+                body: data
+            })
+            
+            const urlHaus = await urlHausReq.json()
+
+            if(urlHaus.query_status !== "no_results") {
+                // oh god! results for a possible threat!!!!
+                content = content.replace(/\{refresh\}/g, `<script>document.documentElement.style.setProperty('--background', "#e33d3d"); document.documentElement.style.setProperty('--color', "#fff");</script>`);
+                // change background color to something scary
+                content = content.replace(/\{titleText\}/g, "WARNING! Possible malicious site ahead!");
+                content = content.replace(/\{description\}/g, "The following URL: "+url+", has been marked as dangerous by <a href='https://urlhaus.abuse.ch/' target='_blank' rel='noopener noreferrer'>URLhaus</a>. Visiting this site could bring harm to you or your computer.");
+                content = content.replace(/\{linkText\}/g, "Accept the potential risk and continue.");
+            }
+        } catch(e)  {
+            console.error("Could not check URLHaus!", e)
+        }
+    }
+
+    // apparently its all fun and games!
     content = content.replace(/\{refresh\}/g, `<meta http-equiv='Refresh' content='0; url=${url}' />`);
     content = content.replace(/\{titleText\}/g, "Redirecting!");
     content = content.replace(/\{description\}/g, "Not working?");
     content = content.replace(/\{linkText\}/g, "Click here to redirect manually.");
-
 
     // 3. Send the modified content as HTML
     res.setHeader('Content-Type', 'text/html');
